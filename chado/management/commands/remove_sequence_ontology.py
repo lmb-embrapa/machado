2d89bc6dab22f438fdf475d022b2d2a843156740
azneto <azneto@gmail.com>
2017-03-24 11:45:01 -0300

    fix to remove only cv.name=sequence

--- chado/management/commands/remove_sequence_ontology.py
+++ chado/management/commands/remove_sequence_ontology.py
@@ -8,7 +8,6 @@ class Command(BaseCommand):
         parser.add_argument("--name", help="cv.name", required = True, type=str)
         parser.add_argument("--definition", help="cv.definition", required = True, type=str)
 
-
     def handle(self, *args, **options):
 
         if Cv.objects.get(name=options['name'],definition=options['definition']):
@@ -16,20 +15,3 @@ class Command(BaseCommand):
             for cvterm in Cvterm.objects.filter(cv=cv):
                 Dbxref.objects.filter(dbxref_id=cvterm.dbxref_id).delete()
             cv.delete()
-
-            cv = Cv.objects.get(name='cvterm_relationship',definition=options['definition'])
-            for cvterm in Cvterm.objects.filter(cv=cv):
-                Dbxref.objects.filter(dbxref_id=cvterm.dbxref_id).delete()
-            cv.delete()
-
-            cv = Cv.objects.get(name='cvterm_property_type',definition=options['definition'])
-            for cvterm in Cvterm.objects.filter(cv=cv):
-                Dbxref.objects.filter(dbxref_id=cvterm.dbxref_id).delete()
-            cv.delete()
-
-            cv = Cv.objects.get(name='synonym_type',definition=options['definition'])
-            for cvterm in Cvterm.objects.filter(cv=cv):
-                Dbxref.objects.filter(dbxref_id=cvterm.dbxref_id).delete()
-            cv.delete()
-
-
