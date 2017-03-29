e0445c5d4b4e8957598d4590553b954ef0bf48c8
Mauricio de Alvarenga Mudadu <m351290@desk6455.cnptia.embrapa.br>
2017-03-29 16:25:18 -0300

    First version of remove_organism.py created. Only genus and species are mandatory.

---
+++ chado/management/commands/remove_organism.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist, FieldDoesNotExist
+from chado.models import Organism
+
+
+class Command(BaseCommand):
+    help = 'Remove organism'
+
+    def add_arguments(self, parser):
+        parser.add_argument("--genus", help="genus", required = True, type=str)
+        parser.add_argument("--species", help="species", required = True, type=str)
+
+    def handle(self, *args, **options):
+
+        species=options['species']
+        genus=options['genus']
+
+        try:
+            organism = Organism.objects.get(species=species,genus=genus)
+            if organism:
+                 organism.delete()
+                 self.stdout.write(self.style.SUCCESS('%s %s removed' % (genus,species)))
+
+        except ObjectDoesNotExist:
+            print('Organism does not exist in database!')
+
