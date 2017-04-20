2ce5c36e2ada2982730245e895247eeb7242914a
azneto <azneto@gmail.com>
2017-04-20 11:02:10 -0300

    PEP8

--- chado/management/commands/load_sequence_ontology.py
+++ chado/management/commands/load_sequence_ontology.py
@@ -1,19 +1,21 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
-from chado.models import Cv,Cvprop,Cvterm,CvtermDbxref,Cvtermprop,CvtermRelationship,Cvtermsynonym,Db,Dbxref
-import networkx
+from chado.models import Cv, Cvterm, CvtermRelationship, Dbxref
 import obonet
-from chado.lib.dbxref import *
-from chado.lib.cvterm import *
+from chado.lib.dbxref import get_set_db, get_set_dbxref
+from chado.lib.cvterm import get_set_cvterm, get_set_cvtermprop
+from chado.lib.cvterm import get_set_cvterm_dbxref, process_cvterm_xref
+from chado.lib.cvterm import process_cvterm_so_synonym, process_cvterm_def
 
 
 class Command(BaseCommand):
     help = 'Load Sequence Ontology'
 
-
     def add_arguments(self, parser):
-        parser.add_argument("--so", help="Sequence Ontology file obo. Available at https://github.com/The-Sequence-Ontology/SO-Ontologies", required = True, type=str)
-
+        parser.add_argument("--so", help="Sequence Ontology file obo."
+                            "Available at https://github.com/"
+                            "The-Sequence-Ontology/SO-Ontologies",
+                            required=True, type=str)
 
     def handle(self, *args, **options):
 
@@ -29,7 +31,9 @@ class Command(BaseCommand):
             cv = Cv.objects.get(name=cv_name)
 
             if cv is not None:
-                self.stdout.write(self.style.ERROR('cv: cannot load %s %s (already registered)' % (cv_name,cv_definition)))
+                self.stdout.write(self.style.ERROR('cv: cannot load %s %s'
+                                                   '(already registered)'
+                                                   % (cv_name, cv_definition)))
 
         except ObjectDoesNotExist:
 
@@ -40,22 +44,36 @@ class Command(BaseCommand):
                                    definition=cv_definition)
             cv.save()
 
-
             # Creating cvterm is_symmetric to be used as type_id in cvtermprop
-            dbxref_is_symmetric = get_set_dbxref('internal','is_symmetric','')
-            cvterm_is_symmetric = get_set_cvterm('cvterm_property_type','is_symmetric','',dbxref_is_symmetric,0)
+            dbxref_is_symmetric = get_set_dbxref('internal',
+                                                 'is_symmetric',
+                                                 '')
+            cvterm_is_symmetric = get_set_cvterm('cvterm_property_type',
+                                                 'is_symmetric', '',
+                                                 dbxref_is_symmetric, 0)
 
             # Creating cvterm is_transitive to be used as type_id in cvtermprop
-            dbxref_is_transitive = get_set_dbxref('internal','is_transitive','')
-            cvterm_is_transitive = get_set_cvterm('cvterm_property_type','is_transitive','',dbxref_is_transitive,0)
-
+            dbxref_is_transitive = get_set_dbxref('internal',
+                                                  'is_transitive',
+                                                  '')
+            cvterm_is_transitive = get_set_cvterm('cvterm_property_type',
+                                                  'is_transitive',
+                                                  '',
+                                                  dbxref_is_transitive,
+                                                  0)
 
             self.stdout.write('Loading typedefs')
 
             # Load typedefs as Dbxrefs and Cvterm
             for typedef in G.graph['typedefs']:
-                dbxref_typedef = get_set_dbxref('_global',typedef['id'],typedef.get('def'))
-                cvterm_typedef = get_set_cvterm(cv.name,typedef.get('id'),typedef.get('def'),dbxref_typedef,1)
+                dbxref_typedef = get_set_dbxref('_global',
+                                                typedef['id'],
+                                                typedef.get('def'))
+                cvterm_typedef = get_set_cvterm(cv.name,
+                                                typedef.get('id'),
+                                                typedef.get('def'),
+                                                dbxref_typedef,
+                                                1)
 
                 # Load is_symmetric
                 if typedef.get('is_symmetric') is not None:
@@ -70,31 +88,42 @@ class Command(BaseCommand):
                                        value=1,
                                        rank=0)
 
-
             self.stdout.write('Loading terms')
 
             # Creating cvterm comment to be used as type_id in cvtermprop
-            dbxref_comment = get_set_dbxref('internal','comment','')
-            cvterm_comment = get_set_cvterm('cvterm_property_type','comment','',dbxref_comment,0)
+            dbxref_comment = get_set_dbxref('internal', 'comment', '')
+            cvterm_comment = get_set_cvterm('cvterm_property_type',
+                                            'comment',
+                                            '',
+                                            dbxref_comment,
+                                            0)
 
-            for n,data in G.nodes(data=True):
+            for n, data in G.nodes(data=True):
 
                 # Save the term to the Dbxref model
-                aux_db,aux_accession = n.split(':')
-                dbxref = get_set_dbxref(aux_db,aux_accession,'')
+                aux_db, aux_accession = n.split(':')
+                dbxref = get_set_dbxref(aux_db, aux_accession, '')
 
                 # Save the term to the Cvterm model
-                cvterm = get_set_cvterm(cv.name,data.get('name'),'',dbxref,0)
+                cvterm = get_set_cvterm(cv.name,
+                                        data.get('name'),
+                                        '',
+                                        dbxref,
+                                        0)
 
                 # Load definition and dbxrefs
-                definition = process_cvterm_def(cvterm,data.get('def'))
+                process_cvterm_def(cvterm, data.get('def'))
 
                 # Load alt_ids
                 if data.get('alt_id'):
                     for alt_id in data.get('alt_id'):
-                        aux_db,aux_accession = alt_id.split(':')
-                        dbxref_alt_id = get_set_dbxref(aux_db,aux_accession,'')
-                        get_set_cvterm_dbxref(cvterm,dbxref_alt_id,0)
+                        aux_db, aux_accession = alt_id.split(':')
+                        dbxref_alt_id = get_set_dbxref(aux_db,
+                                                       aux_accession,
+                                                       '')
+                        get_set_cvterm_dbxref(cvterm,
+                                              dbxref_alt_id,
+                                              0)
 
                 # Load comment
                 if data.get('comment'):
@@ -106,45 +135,53 @@ class Command(BaseCommand):
                 # Load xref
                 if data.get('xref'):
                     for xref in data.get('xref'):
-                        process_cvterm_xref(cvterm,xref)
+                        process_cvterm_xref(cvterm, xref)
 
                 # Load synonyms
                 if data.get('synonym'):
                     for synonym in data.get('synonym'):
-                        process_cvterm_so_synonym(cvterm,synonym)
-
+                        process_cvterm_so_synonym(cvterm, synonym)
 
             self.stdout.write('Loading relationships')
 
             # Creating term is_a to be used as type_id in cvterm_relationship
-            dbxref_is_a = get_set_dbxref('OBO_REL','is_a','')
-            cvterm_is_a = get_set_cvterm('relationship','is_a','',dbxref_is_a,1)
+            dbxref_is_a = get_set_dbxref('OBO_REL', 'is_a', '')
+            cvterm_is_a = get_set_cvterm('relationship',
+                                         'is_a',
+                                         '',
+                                         dbxref_is_a,
+                                         1)
 
-
-            for u,v,type in G.edges(keys=True):
+            for u, v, type in G.edges(keys=True):
 
                 # Get the subject cvterm
-                subject_db_name,subject_dbxref_accession = u.split(':')
+                subject_db_name, subject_dbxref_accession = u.split(':')
                 subject_db = get_set_db(subject_db_name)
-                subject_dbxref = Dbxref.objects.get(db=subject_db,accession=subject_dbxref_accession)
+                subject_dbxref = Dbxref.objects.get(
+                    db=subject_db,
+                    accession=subject_dbxref_accession)
                 subject_cvterm = Cvterm.objects.get(dbxref=subject_dbxref)
 
                 # Get the object cvterm
-                object_db_name,object_dbxref_accession = v.split(':')
+                object_db_name, object_dbxref_accession = v.split(':')
                 object_db = get_set_db(object_db_name)
-                object_dbxref = Dbxref.objects.get(db=object_db,accession=object_dbxref_accession)
+                object_dbxref = Dbxref.objects.get(
+                    db=object_db,
+                    accession=object_dbxref_accession)
                 object_cvterm = Cvterm.objects.get(dbxref=object_dbxref)
 
                 if type == 'is_a':
                     type_cvterm = cvterm_is_a
                 else:
                     type_db = get_set_db('_global')
-                    type_dbxref = Dbxref.objects.get(db=type_db,accession=type)
+                    type_dbxref = Dbxref.objects.get(db=type_db,
+                                                     accession=type)
                     type_cvterm = Cvterm.objects.get(dbxref=type_dbxref)
 
-                cvrel = CvtermRelationship.objects.create(type_id=type_cvterm.cvterm_id,
-                                                          subject_id=subject_cvterm.cvterm_id,
-                                                          object_id=object_cvterm.cvterm_id)
+                cvrel = CvtermRelationship.objects.create(
+                    type_id=type_cvterm.cvterm_id,
+                    subject_id=subject_cvterm.cvterm_id,
+                    object_id=object_cvterm.cvterm_id)
                 cvrel.save()
 
         self.stdout.write(self.style.SUCCESS('Done'))
