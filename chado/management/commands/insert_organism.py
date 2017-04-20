3f551b9f4135f1eebe7e32c52925442b63b9bc69
Mauricio de Alvarenga Mudadu <m351290@desk6455.cnptia.embrapa.br>
2017-04-20 10:42:49 -0300

    Modified Raising error when organism already registered; Organized code to PEP8 certification.

--- chado/management/commands/insert_organism.py
+++ chado/management/commands/insert_organism.py
@@ -1,38 +1,60 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 from chado.models import Organism
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
     help = 'Insert organism'
 
     def add_arguments(self, parser):
-        parser.add_argument("--abbreviation", help="abbreviation", required = False, type=str)
-        parser.add_argument("--genus", help="genus", required = True, type=str)
-        parser.add_argument("--species", help="species", required = True, type=str)
-        parser.add_argument("--common_name", help="common name", required = False, type=str)
-        parser.add_argument("--infraspecific_name", help="infraspecific name", required = False, type=str)
-        parser.add_argument("--type", help="type (Organism_type_Cvterm)", required = False, type=str)
-        parser.add_argument("--comment", help="comment", required = False, type=str)
+        parser.add_argument("--abbreviation",
+                            help="abbreviation",
+                            required=False,
+                            type=str)
+        parser.add_argument("--genus",
+                            help="genus",
+                            required=True,
+                            type=str)
+        parser.add_argument("--species",
+                            help="species",
+                            required=True,
+                            type=str)
+        parser.add_argument("--common_name",
+                            help="common name",
+                            required=False,
+                            type=str)
+        parser.add_argument("--infraspecific_name",
+                            help="infraspecific name",
+                            required=False,
+                            type=str)
+        parser.add_argument("--type",
+                            help="type (Organism_type_Cvterm)",
+                            required=False,
+                            type=str)
+        parser.add_argument("--comment",
+                            help="comment",
+                            required=False,
+                            type=str)
 
     def handle(self, *args, **options):
-
-        species=options['species']
-        genus=options['genus']
+        species = options['species']
+        genus = options['genus']
 
         try:
             spp = Organism.objects.get(species=species, genus=genus)
-            
             if (spp is not None):
-                print('Organism already registered!')
+                raise IntegrityError('Organism already registered!' % spp)
         except ObjectDoesNotExist:
-            organism = Organism.objects.create(abbreviation= options['abbreviation'],
-                                         genus = options['genus'],
-                                         species = options['species'],
-                                         common_name = options['common_name'],
-                                         infraspecific_name = options['infraspecific_name'],
-                                         type = options['type'],
-                                         comment = options['comment'])
+            organism = Organism.objects.create(abbreviation=options['abbrevi'
+                                               'ation'],
+                                               genus=options['genus'],
+                                               species=options['species'],
+                                               common_name=options['common'
+                                               '_name'],
+                                               infraspecific_name=options[
+                                               'infraspecific_name'],
+                                               type=options['type'],
+                                               comment=options['comment'])
             organism.save()
             self.stdout.write(self.style.SUCCESS('%s registered' % species))
-
