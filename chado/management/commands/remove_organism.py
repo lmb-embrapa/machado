73617ed47de3e8496831ed6ca3ac9575be012d7b
azneto <azneto@gmail.com>
2017-04-20 11:04:50 -0300

    PEP8

--- chado/management/commands/remove_organism.py
+++ chado/management/commands/remove_organism.py
@@ -1,5 +1,5 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.core.exceptions import ObjectDoesNotExist, FieldDoesNotExist
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
 from chado.models import Organism
 
 
@@ -7,20 +7,26 @@ class Command(BaseCommand):
     help = 'Remove organism'
 
     def add_arguments(self, parser):
-        parser.add_argument("--genus", help="genus", required = True, type=str)
-        parser.add_argument("--species", help="species", required = True, type=str)
+        parser.add_argument("--genus",
+                            help="genus",
+                            required=True,
+                            type=str)
+        parser.add_argument("--species",
+                            help="species",
+                            required=True,
+                            type=str)
 
     def handle(self, *args, **options):
 
-        species=options['species']
-        genus=options['genus']
+        species = options['species']
+        genus = options['genus']
 
         try:
-            organism = Organism.objects.get(species=species,genus=genus)
+            organism = Organism.objects.get(species=species, genus=genus)
             if organism:
-                 organism.delete()
-                 self.stdout.write(self.style.SUCCESS('%s %s removed' % (genus,species)))
+                organism.delete()
+                self.stdout.write(self.style.SUCCESS('%s %s removed'
+                                                     % (genus, species)))
 
         except ObjectDoesNotExist:
-            print('Organism does not exist in database!')
-
+                print('Organism does not exist in database!')
