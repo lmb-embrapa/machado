b7d5ed00d3fc2984dfeaaab2c8cae43ee384f4c0
azneto <azneto@gmail.com>
2017-04-24 22:08:20 -0300

    initial commit - remove_fasta

---
+++ chado/management/commands/remove_fasta.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+from chado.models import Db
+
+
+class Command(BaseCommand):
+    help = 'Remove Ontology (CASCADE)'
+
+    def add_arguments(self, parser):
+        parser.add_argument("--name", help="cv.name", required=True, type=str)
+
+    def handle(self, *args, **options):
+
+        try:
+            db = Db.objects.get(name=options['name'])
+
+            self.stdout.write('Deleting %s and every child record (CASCADE)'
+                              % (options['name']))
+
+            db.delete()
+
+            self.stdout.write(self.style.SUCCESS('Done'))
+        except ObjectDoesNotExist:
+            self.stdout.write(self.style.ERROR(
+                'Cannot remove %s (not registered)' % (options['name'])))
