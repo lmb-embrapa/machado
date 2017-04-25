42cad34c9022e85e265cd85e3ac6b3c9d0313ab4
Mauricio de Alvarenga Mudadu <m351290@desk6455.cnptia.embrapa.br>
2017-04-25 10:53:40 -0300

    created method for getting/setting organism

--- chado/lib/organism.py
+++ chado/lib/organism.py
@@ -16,3 +16,21 @@ def get_organism(organism):
         raise ObjectDoesNotExist('%s not registered.'
                                  % organism)
     return organism
+
+
+def get_set_organism(organism_name):
+    # receives an organism binomial name with "Genus species" format
+    genus = ""
+    species = ""
+    try:
+        genus, species = organism_name.split(' ')
+    except ValueError:
+        raise ValueError('The organism genus and species should be '
+                         'separated by a single space')
+
+    try:
+        organism = Organism.objects.get(species=species, genus=genus)
+    except ObjectDoesNotExist:
+        organism = Organism.objects.create(genus=genus, species=species)
+        organism.save()
+    return (organism)
