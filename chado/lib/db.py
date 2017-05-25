2fe9615c748dfe6c9a794143e5dd1a3250b52e6c
Mauricio de Alvarenga Mudadu <m351290@desk6455.cnptia.embrapa.br>
2017-05-25 13:26:57 -0300

    moved in get_set_db from dbxref lib

--- chado/lib/db.py
+++ chado/lib/db.py
@@ -1,7 +1,28 @@
 import os
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
-from chado.models import Db
+from chado.models import Db, Dbprop
+
+
+def get_set_db(db_name, **kargs):
+    description = kargs.get('description')
+    urlprefix = kargs.get('urlprefix')
+    url = kargs.get('url')
+
+    try:
+        # Check if the db is already registered
+        db = Db.objects.get(name=db_name)
+        return db
+
+    except ObjectDoesNotExist:
+
+        # Save the name to the Db model
+        db = Db.objects.create(name=db_name,
+                               description=description,
+                               urlprefix=urlprefix,
+                               url=url)
+        db.save()
+        return db
 
 
 def set_db_file(file, **args):
@@ -16,3 +37,22 @@ def set_db_file(file, **args):
                                description=args.get('description'),
                                url=args.get('url'))
     return db
+
+
+def get_set_dbprop(db, cvterm_id, value, rank=0):
+
+    dbprop = ""
+
+    try:
+        # Check if the dbxref is already registered
+        dbprop = Dbprop.objects.get(db=db, type_id=cvterm_id)
+
+    except ObjectDoesNotExist:
+
+        # Save to the Dbxref model
+        dbprop = Dbprop.objects.create(db=db,
+                                       type_id=cvterm_id,
+                                       value=value,
+                                       rank=rank)
+        dbprop.save()
+    return dbprop
