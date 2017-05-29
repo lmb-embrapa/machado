78f233e3ae4bb7728e1cbb5a92d8ef3e97f60619
azneto <azneto@gmail.com>
2017-05-29 15:40:55 -0300

    updating get_organism to search from infraspecific name as well

--- chado/lib/organism.py
+++ chado/lib/organism.py
@@ -5,13 +5,24 @@ from chado.models import Organism, OrganismDbxref
 def get_organism(organism):
 
     try:
-        genus, species = organism.split(' ')
+        aux = organism.split(' ')
+        genus = aux[0]
+        species = 'spp.'
+        infraspecific = ''
+        if len(aux) == 2:
+            species = aux[1]
+        elif len(aux) > 2:
+            species = aux[1]
+            infraspecific = ' '.join(aux[2:])
+
     except ValueError:
         raise ValueError('The organism genus and species should be '
                          'separated by a single space')
 
     try:
-        organism = Organism.objects.get(species=species, genus=genus)
+        organism = Organism.objects.get(species=species,
+                                        genus=genus,
+                                        infraspecific_name=infraspecific)
     except ObjectDoesNotExist:
         raise ObjectDoesNotExist('%s not registered.'
                                  % organism)
