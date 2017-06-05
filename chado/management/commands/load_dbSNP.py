c071cf0d489324e5561b1babd0a5f95e24fe43b5
Mauricio de Alvarenga Mudadu <m351290@desk6455.cnptia.embrapa.br>
2017-06-05 11:05:07 -0300

    implemented cvterm dictionaries for speed loading

--- chado/management/commands/load_dbSNP.py
+++ chado/management/commands/load_dbSNP.py
@@ -3,20 +3,25 @@ from django.core.management.base import BaseCommand
 from chado.lib.dbxref import get_set_dbxref
 from chado.lib.db import set_db_file, get_set_db, get_set_dbprop
 from chado.lib.cvterm import get_set_cv, get_set_cvterm, get_set_cvtermprop
+from chado.lib.cvterm import get_ontology_term
 from chado.lib.organism import get_set_organism
 from chado.lib.project import (get_project)
 from django.core.exceptions import ObjectDoesNotExist
-from chado.models import Feature, Featureprop
+from chado.models import Feature, Featureprop, Featureloc, Db, Dbxref
 from pysam import VariantFile
 
 
 class Command(BaseCommand):
-    help = 'Load NCBIs dbSNP VCF file (use tabix to index file is suggested). '
-    'Following VCF specifications from: https://samtools.github.io/hts-specs/'
+    help = """Load NCBIs dbSNP VCF file (use tabix to index file is suggested).
+              Following VCF specifications from:
+              https://samtools.github.io/hts-specs/"""
 
     cv_name = 'feature_property'
     db_name = 'VCF'
     db_description = 'Variant Call Format'
+    cvterm_snp_name = 'SNP'
+    cv_src_name = 'sequence'
+    cvterm_chromosome_name = 'chromosome'
     # in conformity with VCF versions 4.0, 4.1, 4.2, 4.3...
     # check: https://samtools.github.io/hts-specs/VCFv4.3.pdf
     header_fields = [("CHROM", "chromosome"), ("POS", "position"),
@@ -28,6 +33,8 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("--vcf", help="variant .vcf file", required=True,
                             type=str)
+        parser.add_argument("--fasta", help="ref. fasta file", required=True,
+                            type=str)
         parser.add_argument("--organism", help="organism scientific name"
                             " (Genus species)", required=True, type=str)
         parser.add_argument("--description", help="DB Description",
@@ -36,6 +43,7 @@ class Command(BaseCommand):
                             type=str)
 
     def handle(self, *args, **options):
+
         # retrieve project object
         project = ""
         if options['project']:
@@ -53,6 +61,17 @@ class Command(BaseCommand):
         # get db object
         db_file = set_db_file(file=options['vcf'],
                               description=options.get('description'))
+
+        # REFERENCE FEATURE FOR FEATURE PROP ASSIGNING...
+        # try to get reference feature (fasta file)
+        db_src = ""
+        try:
+            db_src = Db.objects.get(name=options['fasta'])
+        except ObjectDoesNotExist:
+            raise ObjectDoesNotExist(
+                "Reference db (fasta file) does not exist or was not found. "
+                "VCF file should have a reference fasta file assigned in the"
+                " feature table.")
 
         # start reading VCF file, auto-detect input format
         vcf_in = VariantFile(options["vcf"])
@@ -74,6 +93,7 @@ class Command(BaseCommand):
         # ############################################
         # get fileformat and start storing header info
         # ############################################
+        dict_cvterms_info = {}
         self.stdout.write('%s Processing header...' % datetime.now())
         for x in vcf_in.header.records:
             # GENERIC are cvterms for the DB file
@@ -128,6 +148,8 @@ class Command(BaseCommand):
                                                # e.g. "1"
                                                value=x.type,
                                                rank=0)
+                            # store cvterm for further use
+                            dict_cvterms_info[tup[1]] = cvterm
                         else:
                             # create dbxref and cvterm objects for the
                             # property information of the ID terms
@@ -152,26 +174,34 @@ class Command(BaseCommand):
                                                rank=0)
         # start storing data from the record field
         # first, get/set the cvterms for the mandatory 8 fields of the header
+        # create dictionary of header objects for further reuse
+        dict_header_cvterms = {}
         for tuples in self.header_fields:
             # print(tuples)
             # print(VCF_version.value)
             dbxref_header = get_set_dbxref(db_name=db_cv.name,
-                                           # e.g. "RSPOS"
+                                           # e.g. "POS"
                                            accession=tuples[0],
-                                           # e.g. "GENERIC"
+                                           # e.g. "VCF4.0"
                                            version=VCF_version.value,
-                                           # e.g. "dbSNP_BUILD_ID"
+                                           # e.g. "position"
                                            description=tuples[1])
 
-            get_set_cvterm(cv_name=cv.name,
+            cvterm_header = get_set_cvterm(
+                           cv_name=cv.name,
                            cvterm_name=tuples[0],
                            definition=tuples[1],
                            dbxref=dbxref_header,
                            is_relationshiptype=0)
-
+            # set dictionary of cvterm objects
+            dict_header_cvterms[tuples[0]] = cvterm_header
         counter = 0
         self.stdout.write(self.style.SUCCESS('%s Done'
                                              % datetime.now()))
+        dbxref_src = None
+        feature_src = None
+        # retrieve cvterm for SNP (sequence ontology)
+        cvterm_src = get_ontology_term(ontology='sequence', term='SNP')
         # start iterating through record data fields to store features
         self.stdout.write('%s Processing records...' % datetime.now())
         for rec in vcf_in.fetch():
@@ -181,22 +211,29 @@ class Command(BaseCommand):
             if not counter % 1000:
                 self.stdout.write('%s - %s features processed.'
                                   % (datetime.now(), counter))
-
+            # split record line in fields separated by spaces
             fields = str(rec).split()
+
+            # get dbxref_src for featureprop assignment
+            # (retrieve the 'chromosome number' feature associated to the SNP)
+            if (not dbxref_src):
+                dbxref_src = Dbxref.objects.get(db_id=db_src,
+                                                accession=fields[0])
+            # set feature_scr object ('chromosome number')
+            if (not feature_src):
+                feature_src = Feature.objects.get(dbxref=dbxref_src,
+                                                  uniquename=fields[0])
+
             # get/set dbxref for SNP ID
             dbxref_rec = get_set_dbxref(db_name=db_file.name,
                                         accession=fields[2],
                                         project=project)
-            # retrieve cvterm "ID"
-            cvterm_rec = get_set_cvterm(cv_name=cv.name,
-                                        cvterm_name="ID",
-                                        dbxref=dbxref_rec)
-            # creating a new feature
+            # creating a new SNP ID feature
             feature_rec = Feature.objects.create(dbxref=dbxref_rec,
                                                  organism=organism,
                                                  name=fields[2],
                                                  uniquename=fields[2],
-                                                 type_id=cvterm_rec.cvterm_id,
+                                                 type_id=cvterm_src.cvterm_id,
                                                  is_analysis=False,
                                                  is_obsolete=False,
                                                  timeaccessioned=datetime.
@@ -204,24 +241,47 @@ class Command(BaseCommand):
                                                  timelastmodified=datetime.
                                                  now(timezone.utc)
                                                  )
-            # load feature properties (do not need 2 (ID). 7 (INFO) will be
-            # parsed later on...
-            for i in range(7):
-                # retrieve cvterm
-                # print(self.header_fields[i][0])
-                cvterm_field = get_set_cvterm(
-                        cv_name=cv.name,
-                        cvterm_name=self.header_fields[i][0],
-                        dbxref=dbxref_rec
-                        )
-                Featureprop.objects.create(
-                              feature=feature_rec,
-                              type_id=cvterm_field.cvterm_id,
-                              value=fields[i],
-                              rank=0
-                              )
-            # parse 8th field (INFO) and load feature properties
-            # print(fields[7])
+            # set position of the SNP
+            Featureloc.objects.create(
+                                      feature=feature_rec,
+                                      srcfeature_id=feature_src.feature_id,
+                                      fmin=fields[1],
+                                      is_fmin_partial=False,
+                                      fmax=fields[1],
+                                      is_fmax_partial=False,
+                                      strand=0,
+                                      phase=0,
+                                      locgroup=0,
+                                      rank=0)
+            # set SNP REF property
+            Featureprop.objects.create(
+                          feature=feature_rec,
+                          type_id=dict_header_cvterms['REF'].cvterm_id,
+                          value=fields[3],
+                          rank=0
+                          )
+            # set SNP ALT property
+            Featureprop.objects.create(
+                          feature=feature_rec,
+                          type_id=dict_header_cvterms['ALT'].cvterm_id,
+                          value=fields[4],
+                          rank=0
+                          )
+            # set SNP QUAL property
+            Featureprop.objects.create(
+                          feature=feature_rec,
+                          type_id=dict_header_cvterms['QUAL'].cvterm_id,
+                          value=fields[5],
+                          rank=0
+                          )
+            # set SNP FILTER property
+            Featureprop.objects.create(
+                          feature=feature_rec,
+                          type_id=dict_header_cvterms['FILTER'].cvterm_id,
+                          value=fields[6],
+                          rank=0
+                          )
+            # start parsing INFO field
             info_fields = fields[7].split(";")
             # print(info_fields)
             for field in info_fields:
@@ -229,17 +289,11 @@ class Command(BaseCommand):
                 key_value = field.split("=")
                 if len(key_value) == 1:
                     key_value.append("0")
-                # print(key_value)
-                cvterm_info = get_set_cvterm(cv_name=cv.name,
-                                             cvterm_name=key_value[0],
-                                             dbxref=dbxref_rec
-                                             )
                 Featureprop.objects.create(
-                                           feature=feature_rec,
-                                           type_id=cvterm_info.cvterm_id,
-                                           value=key_value[1],
-                                           rank=0
-                                           )
-
+                             feature=feature_rec,
+                             type_id=dict_cvterms_info[key_value[0]].cvterm_id,
+                             value=key_value[1],
+                             rank=0
+                             )
         self.stdout.write(self.style.SUCCESS('%s Done'
                                              % datetime.now()))
