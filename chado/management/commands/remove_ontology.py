4b5f5bbf1f54470f80d8f3277fc67bfc62369b45
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2018-01-24 14:59:57 -0200

    Delete cascade doesn't work anymore. Deleting every child record.

--- chado/management/commands/remove_ontology.py
+++ chado/management/commands/remove_ontology.py
@@ -1,16 +1,21 @@
+"""Remove ontology."""
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
-from chado.models import Cv, Cvterm, CvtermDbxref, Dbxref
+from chado.models import Cv, Cvterm, CvtermDbxref, Cvtermprop, Cvtermsynonym
+from chado.models import Dbxref
 
 
 class Command(BaseCommand):
+    """Remove ontology."""
+
     help = 'Remove Ontology (CASCADE)'
 
     def add_arguments(self, parser):
+        """Define the arguments."""
         parser.add_argument("--name", help="cv.name", required=True, type=str)
 
     def handle(self, *args, **options):
-
+        """Execute the main function."""
         try:
             cv = Cv.objects.get(name=options['name'])
 
@@ -21,6 +26,10 @@ class Command(BaseCommand):
                                                                   flat=True)
             dbxref_ids = CvtermDbxref.objects.filter(
                 cvterm_id__in=cvterm_ids).values_list('dbxref_id', flat=True)
+            CvtermDbxref.objects.filter(cvterm_id__in=cvterm_ids).delete()
+            Cvtermsynonym.objects.filter(cvterm_id__in=cvterm_ids).delete()
+            Cvtermprop.objects.filter(cvterm_id__in=cvterm_ids).delete()
+            Cvterm.objects.filter(cvterm_id__in=cvterm_ids).delete()
             Dbxref.objects.filter(dbxref_id__in=dbxref_ids).delete()
 
             dbxref_ids = Cvterm.objects.filter(
