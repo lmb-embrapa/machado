b32427e2d53dbd98e1f1874bab66b2c3ab6003f0
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2018-01-24 15:38:16 -0200

    Delete cascade doesn't work anymore. Deleting every child record.

--- chado/management/commands/remove_ontology.py
+++ chado/management/commands/remove_ontology.py
@@ -1,7 +1,9 @@
 """Remove ontology."""
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
-from chado.models import Cv, Cvterm, CvtermDbxref, Cvtermprop, Cvtermsynonym
+from chado.models import Cv, Cvterm
+from chado.models import CvtermDbxref, Cvtermprop
+from chado.models import Cvtermsynonym, CvtermRelationship
 from chado.models import Dbxref
 
 
@@ -29,6 +31,10 @@ class Command(BaseCommand):
             CvtermDbxref.objects.filter(cvterm_id__in=cvterm_ids).delete()
             Cvtermsynonym.objects.filter(cvterm_id__in=cvterm_ids).delete()
             Cvtermprop.objects.filter(cvterm_id__in=cvterm_ids).delete()
+            CvtermRelationship.objects.filter(
+                object_id__in=cvterm_ids).delete()
+            CvtermRelationship.objects.filter(
+                subject_id__in=cvterm_ids).delete()
             Cvterm.objects.filter(cvterm_id__in=cvterm_ids).delete()
             Dbxref.objects.filter(dbxref_id__in=dbxref_ids).delete()
 
