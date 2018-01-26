b1d26faaff6fbabc7f4dd732e668ae6c9b67353e
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2018-01-26 15:55:50 -0200

    deleting get_set_project, get_set_project_dbxref, and get_set_project_feature

--- chado/lib/dbxref.py
+++ chado/lib/dbxref.py
@@ -1,12 +1,9 @@
 """dbxref library."""
-from cachetools import cached
-from chado.models import Db, Dbxref
-from chado.lib.project import get_set_project_dbxref
+from chado.models import Db, Dbxref, ProjectDbxref
 from chado.lib.db import get_set_db
 from django.core.exceptions import ObjectDoesNotExist
 
 
-@cached(cache={})
 def get_set_dbxref(db_name, accession, **kargs):
     """Create/Retrieve dbxref object."""
     # Get/Set Db instance: db
@@ -20,8 +17,8 @@ def get_set_dbxref(db_name, accession, **kargs):
         # Check if the dbxref is already registered
         dbxref = Dbxref.objects.get(db=db, accession=accession)
         if project:
-            get_set_project_dbxref(dbxref=dbxref,
-                                   project=project)
+            ProjectDbxref.objects.create(dbxref=dbxref,
+                                         project=project)
         return dbxref
 
     except ObjectDoesNotExist:
@@ -35,12 +32,11 @@ def get_set_dbxref(db_name, accession, **kargs):
 
         dbxref.save()
         if project:
-            get_set_project_dbxref(dbxref=dbxref,
-                                   project=project)
+            ProjectDbxref.objects.create(dbxref=dbxref,
+                                         project=project)
         return dbxref
 
 
-@cached(cache={})
 def get_dbxref(db_name, accession):
     """Retrieve dbxref object."""
     # Get/Set Db instance: db
--- chado/lib/project.py
+++ chado/lib/project.py
@@ -1,48 +1,13 @@
+"""project library."""
 from django.core.exceptions import ObjectDoesNotExist
-from chado.models import Project, ProjectDbxref, ProjectFeature
+from chado.models import Project
 
 
 def get_project(project_name):
-
+    """Retrieve project object."""
     try:
         project = Project.objects.get(name=project_name)
     except ObjectDoesNotExist:
         raise ObjectDoesNotExist('%s not registered.'
                                  % project_name)
     return project
-
-
-def get_set_project_dbxref(dbxref, project):
-
-    try:
-        project_dbxref = ProjectDbxref.objects.get(project=project,
-                                                   dbxref=dbxref)
-    except ObjectDoesNotExist:
-        # will always set iscurrent to True... change this in the future if it
-        # may. Check the field REMARKS in the table to see what this is about
-        project_dbxref = ProjectDbxref.objects.create(project=project,
-                                                      dbxref=dbxref,
-                                                      is_current=True)
-        project_dbxref.save()
-    return project_dbxref
-
-
-def get_set_project_feature(feature, project):
-
-    try:
-        project_feature = ProjectFeature.objects.get(project=project,
-                                                     feature=feature)
-    except ObjectDoesNotExist:
-        project_feature = ProjectFeature.objects.create(project=project,
-                                                        feature=feature)
-        project_feature.save()
-    return project_feature
-
-
-def get_set_project(project_name):
-    try:
-        project = Project.objects.get(name=project_name)
-    except ObjectDoesNotExist:
-        project = Project.objects.create(name=project_name)
-        project.save()
-    return (project)
