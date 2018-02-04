95cf3bbd386360c98cee0015aec1b9763c6fff8a
azneto <azneto@gmail.com>
2018-02-04 11:23:09 -0200

    refactoring: eliminate relations, sequence, and gene ontology from loaders, in favor of a single ontology class

--- chado/management/commands/load_gene_ontology.py
+++ chado/management/commands/load_gene_ontology.py
@@ -1,8 +1,12 @@
 """Load Gene Ontology."""
 
-from chado.loaders.exceptions import ImportingError
-from chado.loaders.geneontology import GeneOntologyLoader
-from django.core.management.base import BaseCommand, CommandError
+from chado.loaders.common import Validator
+from chado.loaders.ontology import OntologyLoader
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from django.core.management.base import BaseCommand
+from multiprocessing import Lock
+from obonet import read_obo
+from tqdm import tqdm
 
 
 class Command(BaseCommand):
@@ -28,10 +32,62 @@ class Command(BaseCommand):
         if options.get('verbosity'):
             verbosity = options.get('verbosity')
 
-        try:
-            importer = GeneOntologyLoader(verbosity, self.stdout)
-            importer.handle(file, cpu)
-        except ImportingError as e:
-            raise CommandError(str(e))
+        Validator().validate(file)
+
+        # Load the ontology file
+        with open(file) as obo_file:
+            G = read_obo(obo_file)
+
+        cv_definition = G.graph['date']
+
+        if verbosity > 0:
+            self.stdout.write('Preprocessing')
+
+        # Instantiating Ontology in order to have access to secondary cv, db,
+        # cvterm, and dbxref, even though the main cv will not be used.
+        ontology = OntologyLoader('biological_process', cv_definition)
+        ontology = OntologyLoader('molecular_function', cv_definition)
+        ontology = OntologyLoader('cellular_component', cv_definition)
+        ontology = OntologyLoader('external', cv_definition)
+
+        # Load typedefs as Dbxrefs and Cvterm
+        if verbosity > 0:
+            self.stdout.write(
+                'Loading typedefs ({} threads)'.format(cpu))
+
+        pool = ThreadPoolExecutor(max_workers=cpu)
+        tasks = list()
+        for typedef in G.graph['typedefs']:
+            tasks.append(pool.submit(ontology.store_type_def, typedef))
+        for task in tqdm(as_completed(tasks), total=len(tasks)):
+            if task.result():
+                raise(task.result())
+
+        # Load the cvterms
+        if verbosity > 0:
+            self.stdout.write(
+                'Loading terms ({} threads)'.format(cpu))
+
+        lock = Lock()
+        tasks = list()
+        for n, data in G.nodes(data=True):
+            tasks.append(pool.submit(
+                ontology.store_term, n, data, lock))
+        for task in tqdm(as_completed(tasks), total=len(tasks)):
+            if task.result():
+                raise(task.result())
+
+        # Load the relationship between cvterms
+        if verbosity > 0:
+            self.stdout.write(
+                'Loading relationships ({} threads)'.format(cpu))
+
+        tasks = list()
+        for u, v, type in G.edges(keys=True):
+            tasks.append(pool.submit(
+                ontology.store_relationship, u, v, type))
+        for task in tqdm(as_completed(tasks), total=len(tasks)):
+            if task.result():
+                raise(task.result())
 
         self.stdout.write(self.style.SUCCESS('Done'))
