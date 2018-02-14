fc913117a8533771927bb2bbe4bceffec8a21a2c
azneto <azneto@gmail.com>
2018-02-13 22:20:18 -0200

    automatically create a protein feature for each mRNA

--- chado/loaders/feature.py
+++ chado/loaders/feature.py
@@ -26,13 +26,11 @@ class FeatureLoader(object):
 
     def __init__(self, filename, organism, *args, **kwargs):
         """Execute the init function."""
-        # Retrieve organism object
         try:
             self.organism = retrieve_organism(organism)
         except ObjectDoesNotExist as e:
             raise ImportingError(e)
 
-        # Save DB file info
         try:
             self.db = Db.objects.create(name=filename,
                                         description=kwargs.get('description'),
@@ -40,7 +38,6 @@ class FeatureLoader(object):
         except IntegrityError as e:
             raise ImportingError(e)
 
-        # retrieving the pub 'null'
         self.db_null, created = Db.objects.get_or_create(name='null')
         null_dbxref, created = Dbxref.objects.get_or_create(
             db=self.db_null, accession='null')
@@ -58,6 +55,8 @@ class FeatureLoader(object):
             type_id=null_cvterm.cvterm_id,
             is_obsolete=False)
 
+        self.aa_cvterm = retrieve_ontology_term(ontology='sequence',
+                                                term='amino_acid')
         # initialization of lists/sets to store ignored attributes,
         # ignored goterms, and relationships
         self.ignored_attrs = set()
@@ -66,8 +65,6 @@ class FeatureLoader(object):
 
     def get_attributes(self, attributes):
         """Get attributes."""
-        # receive a line from tbx.fetch and retreive one of the attribute
-        # fields
         result = dict()
         fields = attributes.split(";")
         for field in fields:
@@ -82,7 +79,6 @@ class FeatureLoader(object):
             feature: type object
             attrs: type dict
         """
-        # retrieving the cvterm 'exact'
         try:
             cvterm_exact = retrieve_ontology_term('synonym_type', 'exact')
         except ObjectDoesNotExist as e:
@@ -114,17 +110,13 @@ class FeatureLoader(object):
                 for dbxref in dbxrefs:
                     # It expects just one dbxref formated as XX:012345
                     aux_db, aux_dbxref = dbxref.split(':')
-                    # create a dbxref for the column source
                     db, created = Db.objects.get_or_create(name=aux_db)
                     dbxref, created = Dbxref.objects.get_or_create(
                         db=db, accession=aux_dbxref)
-
-                    # associate feature with source
                     FeatureDbxref.objects.create(feature=feature,
                                                  dbxref=dbxref,
                                                  is_current=1)
             elif key in ['alias']:
-                # Store in featuresynonym
                 synonym, created = Synonym.objects.get_or_create(
                     name=attrs.get(key),
                     defaults={'type_id': cvterm_exact.cvterm_id,
@@ -135,7 +127,6 @@ class FeatureLoader(object):
                                               is_current=True,
                                               is_internal=False)
             else:
-                # Store in featureprop
                 note_dbxref, created = Dbxref.objects.get_or_create(
                     db=self.db_null, accession=key)
                 cv_feature_property, created = Cv.objects.get_or_create(
@@ -156,25 +147,21 @@ class FeatureLoader(object):
 
     def store_tabix_feature(self, tabix_feature):
         """Store tabix feature."""
-        # populate tables related to GFF
         attrs = self.get_attributes(tabix_feature.attributes)
         for key in attrs:
             if key not in VALID_ATTRS and key not in ['id', 'name', 'parent']:
                 self.ignored_attrs.add(key)
 
-        # Retrieve sequence ontology object
         cvterm = retrieve_ontology_term(ontology='sequence',
                                         term=tabix_feature.feature)
 
-        # set id = auto#
+        # set id = auto# for features that lack it
         if attrs.get('id') is None:
             attrs['id'] = 'auto{}'.format(str(time()))
 
-        # creating a dbxref for the feature
         dbxref, created = Dbxref.objects.get_or_create(
             db=self.db, accession=attrs['id'])
 
-        # creating a new feature
         try:
             feature = Feature.objects.create(
                     organism=self.organism,
@@ -190,7 +177,6 @@ class FeatureLoader(object):
             raise ImportingError(
                     'ID {} already registered. {}'.format(attrs.get('id'), e))
 
-        # retrieving the source feature
         try:
             srcfeature = Feature.objects.get(
                 uniquename=tabix_feature.contig, organism=self.organism)
@@ -217,7 +203,6 @@ class FeatureLoader(object):
         except ValueError:
             phase = None
 
-        # storing the feature location
         try:
             Featureloc.objects.get_or_create(
                 feature=feature,
@@ -239,26 +224,45 @@ class FeatureLoader(object):
                   phase)
             raise ImportingError(e)
 
-        # adding attributes to featureprop
         self.process_attributes(feature, attrs)
 
-        # storing the feature and parent ids
         if attrs.get('parent') is not None:
-            self.relationships.append((attrs['id'], attrs['parent']))
+            self.relationships.append({'object_id': attrs['id'],
+                                       'subject_id': attrs['parent']})
+
+        # Additional protrein record for each mRNA with the exact same ID
+        if tabix_feature.feature == 'mRNA':
+            translation_of = retrieve_ontology_term(ontology='sequence',
+                                                    term='translation_of')
+            feature_mRNA_translation = Feature.objects.create(
+                    organism=self.organism,
+                    uniquename=attrs.get('id'),
+                    type_id=self.aa_cvterm.cvterm_id,
+                    name=attrs.get('name'),
+                    dbxref=dbxref,
+                    is_analysis=False,
+                    is_obsolete=False,
+                    timeaccessioned=datetime.now(timezone.utc),
+                    timelastmodified=datetime.now(timezone.utc))
+            FeatureRelationship.objects.create(object=feature_mRNA_translation,
+                                               subject=feature,
+                                               type=translation_of,
+                                               rank=0)
 
     def store_relationships(self):
         """Store the relationships."""
-        # Retrieve the part_of relations ontology term
         part_of = retrieve_ontology_term(ontology='sequence',
                                          term='part_of')
-        # creating the feature relationships
         relationships = list()
         for item in self.relationships:
             try:
-                object = Feature.objects.get(uniquename=item[0],
-                                             organism=self.organism)
-                subject = Feature.objects.get(uniquename=item[1],
-                                              organism=self.organism)
+                # the aa features should be excluded since they were created
+                # using the same mRNA ID
+                features = Feature.objects.exclude(type=self.aa_cvterm)
+                object = features.get(uniquename=item['object_id'],
+                                      organism=self.organism)
+                subject = features.get(uniquename=item['subject_id'],
+                                       organism=self.organism)
                 relationships.append(FeatureRelationship(
                     subject_id=subject.feature_id,
                     object_id=object.feature_id,
