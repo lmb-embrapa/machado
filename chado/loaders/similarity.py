37c1e962ae55223ec3131923668a471228557738
azneto <azneto@gmail.com>
2018-02-13 22:20:54 -0200

    handle different sequence types

--- chado/loaders/similarity.py
+++ chado/loaders/similarity.py
@@ -1,6 +1,7 @@
 """Load similarity file."""
 
 from chado.models import Analysis, Analysisfeature, Feature, Featureloc
+from chado.loaders.common import retrieve_ontology_term
 from chado.loaders.exceptions import ImportingError
 from datetime import datetime, timezone
 from django.core.exceptions import ObjectDoesNotExist
@@ -12,17 +13,25 @@ class SimilarityLoader(object):
 
     help = 'Load simlarity records.'
 
-    def __init__(self, filename, program, programversion, *args, **kwargs):
+    def __init__(self, filename, program, programversion, so_query, so_subject,
+                 *args, **kwargs):
         """Execute the init function."""
         try:
+            self.so_term_query = retrieve_ontology_term(
+                    ontology='sequence', term=so_query)
+            self.so_term_subject = retrieve_ontology_term(
+                    ontology='sequence', term=so_subject)
             self.analysis = Analysis.objects.create(
                     algorithm=kwargs.get('algorithm'),
+                    name=kwargs.get('name'),
                     description=kwargs.get('description'),
-                    name=filename,
+                    sourcename=filename,
                     program=program,
                     programversion=programversion,
                     timeexecuted=datetime.now(timezone.utc))
         except IntegrityError as e:
+            raise ImportingError(e)
+        except ObjectDoesNotExist as e:
             raise ImportingError(e)
 
     def retrieve_id_from_description(self, description):
@@ -40,11 +49,13 @@ class SimilarityLoader(object):
         """Store bio_blast_record record."""
         try:
             query_id = record.query.split(' ')[0]
-            query_feature = Feature.objects.get(uniquename=query_id)
+            query_feature = Feature.objects.get(
+                    uniquename=query_id, type=self.so_term_query)
         except ObjectDoesNotExist as e1:
             try:
                 query_id = self.retrieve_id_from_description(record.query)
-                query_feature = Feature.objects.get(uniquename=query_id)
+                query_feature = Feature.objects.get(
+                        uniquename=query_id, type=self.so_term_query)
             except ObjectDoesNotExist as e2:
                 raise ImportingError(e1, e2)
 
@@ -54,11 +65,13 @@ class SimilarityLoader(object):
 
         try:
             subject_id = alignment.title.split(' ')[0]
-            subject_feature = Feature.objects.get(uniquename=subject_id)
+            subject_feature = Feature.objects.get(
+                    uniquename=subject_id, type=self.so_term_subject)
         except ObjectDoesNotExist as e1:
             try:
                 subject_id = self.retrieve_id_from_description(alignment.title)
-                subject_feature = Feature.objects.get(uniquename=subject_id)
+                subject_feature = Feature.objects.get(
+                        uniquename=subject_id, type=self.so_term_subject)
             except ObjectDoesNotExist as e2:
                 raise ImportingError(e1, e2)
 
