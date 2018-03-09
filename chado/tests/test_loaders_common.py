4e57b4dcc1c24e27997e2e7a8ca436d943be395f
mmudado@gmail.com <m351290@desk6455.cnptia.embrapa.br>
2018-03-09 10:42:50 -0300

    Tests: dismantled test_loaders.py into specific test files

---
+++ chado/tests/test_loaders_common.py
@@ -0,0 +1,70 @@
+"""Tests Loaders - Common."""
+
+from chado.loaders.common import insert_organism, retrieve_organism
+from chado.loaders.common import retrieve_ontology_term
+from chado.models import Cv, Cvterm, Db, Dbxref, Organism
+from django.test import TestCase
+
+
+class CommonTest(TestCase):
+    """Tests Loaders - Common."""
+
+    def test_insert_organism_1(self):
+        """Tests - insert_organism 1."""
+        insert_organism(genus='Mus', species='musculus')
+        test_organism_1 = Organism.objects.get(genus='Mus',
+                                               species='musculus')
+        self.assertEqual('Mus', test_organism_1.genus)
+        self.assertEqual('musculus', test_organism_1.species)
+
+    def test_insert_organism_2(self):
+        """Tests - insert_organism 2."""
+        test_db = Db.objects.create(name='test_db')
+        test_dbxref = Dbxref.objects.create(accession='test_dbxref',
+                                            db=test_db)
+
+        test_cv = Cv.objects.create(name='test_cv')
+        Cvterm.objects.create(name='test_cvterm',
+                              cv=test_cv,
+                              dbxref=test_dbxref,
+                              is_obsolete=0,
+                              is_relationshiptype=0)
+        insert_organism(genus='Homo',
+                        species='sapiens',
+                        abbreviation='hs',
+                        common_name='human',
+                        comment='no comments',
+                        type='test_cvterm')
+        test_organism_2 = Organism.objects.get(genus='Homo',
+                                               species='sapiens')
+        self.assertEqual('Homo', test_organism_2.genus)
+        self.assertEqual('sapiens', test_organism_2.species)
+        self.assertEqual('hs', test_organism_2.abbreviation)
+        self.assertEqual('human', test_organism_2.common_name)
+        self.assertEqual('no comments', test_organism_2.comment)
+
+    def test_retrieve_organism(self):
+        """Tests - retrieve organism."""
+        insert_organism(genus="Bos")
+        insert_organism(genus="Bos", species="taurus")
+        insert_organism(genus="Bos", species="taurus",
+                        infraspecific_name="indicus")
+        test_organism = retrieve_organism("Bos")
+        self.assertEqual('Bos', test_organism.genus)
+        test_organism = retrieve_organism("Bos taurus")
+        self.assertEqual('taurus', test_organism.species)
+        test_organism = retrieve_organism("Bos taurus indicus")
+        self.assertEqual('indicus', test_organism.infraspecific_name)
+
+    def test_retrieve_ontology_term(self):
+        """Tests - retrieve ontology term."""
+        test_db = Db.objects.create(name='sequence')
+        test_dbxref = Dbxref.objects.create(db=test_db, accession='nucleotide')
+        test_cv = Cv.objects.create(name='sequence')
+        Cvterm.objects.create(
+            cv=test_cv, dbxref=test_dbxref, name='nucleotide',
+            is_obsolete=0, is_relationshiptype=0)
+        test_ontology_cvterm = retrieve_ontology_term('sequence', 'nucleotide')
+        test_ontology_cv = Cv.objects.get(cv_id=test_ontology_cvterm.cv_id)
+        self.assertEqual('sequence', test_ontology_cv.name)
+        self.assertEqual('nucleotide', test_ontology_cvterm.name)
---
+++ chado/tests/test_loaders_similarity.py
@@ -0,0 +1,124 @@
+"""Tests similarity data loader."""
+
+from chado.models import Analysis, Analysisfeature
+from chado.models import Cv, Cvterm, Db, Dbxref, Organism
+from chado.models import Feature, Featureloc
+from chado.loaders.similarity import SimilarityLoader
+from datetime import datetime, timezone
+from django.test import TestCase
+
+
+class SimilarityTest(TestCase):
+    """Tests Loaders - SimilarityLoader."""
+
+    def test_store_bio_blast_record(self):
+        """Tests - __init__ and store_bio_blast_record."""
+        test_organism = Organism.objects.create(
+            genus='Mus', species='musculus')
+        # creating test SO term
+        test_db = Db.objects.create(name='SO')
+        test_dbxref = Dbxref.objects.create(accession='12345', db=test_db)
+        test_cv = Cv.objects.create(name='sequence')
+        test_gene_term = Cvterm.objects.create(
+            name='gene', cv=test_cv, dbxref=test_dbxref,
+            is_obsolete=0, is_relationshiptype=0)
+        test_dbxref = Dbxref.objects.create(accession='123456', db=test_db)
+        test_aa_term = Cvterm.objects.create(
+            name='polypeptide', cv=test_cv, dbxref=test_dbxref,
+            is_obsolete=0, is_relationshiptype=0)
+        test_dbxref = Dbxref.objects.create(accession='1234567', db=test_db)
+        Cvterm.objects.create(
+            name='match_part', cv=test_cv, dbxref=test_dbxref,
+            is_obsolete=0, is_relationshiptype=0)
+
+        # creating test features
+        Feature.objects.create(
+            organism=test_organism, uniquename='feat1', is_analysis=False,
+            type_id=test_gene_term.cvterm_id, is_obsolete=False,
+            timeaccessioned=datetime.now(timezone.utc),
+            timelastmodified=datetime.now(timezone.utc))
+        Feature.objects.create(
+            organism=test_organism, uniquename='feat2', is_analysis=False,
+            type_id=test_aa_term.cvterm_id, is_obsolete=False,
+            timeaccessioned=datetime.now(timezone.utc),
+            timelastmodified=datetime.now(timezone.utc))
+        test_feat = Feature.objects.create(
+            organism=test_organism, uniquename='feat3', is_analysis=False,
+            type_id=test_aa_term.cvterm_id, is_obsolete=False,
+            timeaccessioned=datetime.now(timezone.utc),
+            timelastmodified=datetime.now(timezone.utc))
+
+        # create a Bio.Blast.Record.Blast
+        class BlastRecord(object):
+            """mock Blast Record."""
+
+        # create a Bio.Blast.Record.Alignment
+        class BlastAlignment(object):
+            """mock Alignment Record."""
+
+        # create a Bio.Blast.Record.HSP
+        class BlastHSP(object):
+            """mock HSP Record."""
+
+        test_HSP1 = BlastHSP()
+        test_HSP1.identities = 98.0
+        test_HSP1.score = 1234
+        test_HSP1.expect = 0.0
+        test_HSP1.query_start = 110
+        test_HSP1.query_end = 1100
+        test_HSP1.sbjct_start = 100
+        test_HSP1.sbjct_end = 1000
+        test_HSP2 = BlastHSP()
+        test_HSP2.identities = 98.2
+        test_HSP2.score = 2234
+        test_HSP2.expect = 0.00000001
+        test_HSP2.query_start = 200
+        test_HSP2.query_end = 2000
+        test_HSP2.sbjct_start = 220
+        test_HSP2.sbjct_end = 2200
+        test_HSP3 = BlastHSP()
+        test_HSP3.identities = 93.2
+        test_HSP3.score = 3234
+        test_HSP3.expect = 0.00000003
+        test_HSP3.query_start = 300
+        test_HSP3.query_end = 3000
+        test_HSP3.sbjct_start = 330
+        test_HSP3.sbjct_end = 3300
+
+        test_alignment1 = BlastAlignment()
+        test_alignment1.title = 'feat2.RNA ID=feat2'
+        test_alignment1.hsps = list()
+        test_alignment1.hsps.append(test_HSP1)
+        test_alignment2 = BlastAlignment()
+        test_alignment2.title = 'feat3.RNA ID=feat3'
+        test_alignment2.hsps = list()
+        test_alignment1.hsps.append(test_HSP2)
+        test_alignment2.hsps.append(test_HSP3)
+
+        test_record = BlastRecord()
+        test_record.query = 'feat1 ID=feat1 moltype=DNA'
+        test_record.alignments = list()
+        test_record.alignments.append(test_alignment1)
+        test_record.alignments.append(test_alignment2)
+
+        test_blast_file = SimilarityLoader(
+                filename='similarity.file',
+                algorithm='smith-waterman',
+                description='command-line example',
+                program='blastx',
+                programversion='2.2.31+',
+                so_query='gene',
+                so_subject='polypeptide')
+
+        test_blast_file.store_bio_blast_record(test_record)
+
+        test_analysis = Analysis.objects.get(sourcename='similarity.file')
+        self.assertEqual('blastx', test_analysis.program)
+
+        test_featureloc = Featureloc.objects.get(
+                srcfeature=test_feat, rank=1)
+
+        test_analysisfeature = Analysisfeature.objects.get(
+                analysis=test_analysis,
+                feature_id=test_featureloc.feature_id)
+        self.assertEqual(3234.0, test_analysisfeature.rawscore)
