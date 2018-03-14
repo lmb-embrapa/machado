7f199cfc1bfd2933ad0472bc8fecd4a14768305b
azneto <azneto@gmail.com>
2018-03-14 17:09:28 -0300

    adding counts: chromosomes, scaffolds, and genes

--- chado/views.py
+++ chado/views.py
@@ -4,7 +4,7 @@ from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from chado.models import Cvterm
+from chado.models import Cvterm, Feature
 
 
 def index(request):
@@ -19,8 +19,26 @@ def stats(request):
     cv = cv.filter(cv_id__count__gt=5)
     cv = cv.order_by('cv__name')
 
+    chr_cvterm = Cvterm.objects.get(cv__name='sequence', name='chromosome')
+    chrs = Feature.objects.values('organism__genus', 'organism__species')
+    chrs = chrs.filter(type_id=chr_cvterm.cvterm_id)
+    chrs = chrs.annotate(Count('organism_id'))
+
+    scaff_cvterm = Cvterm.objects.get(cv__name='sequence', name='assembly')
+    scaffs = Feature.objects.values('organism__genus', 'organism__species')
+    scaffs = scaffs.filter(type_id=scaff_cvterm.cvterm_id)
+    scaffs = scaffs.annotate(Count('organism_id'))
+
+    gene_cvterm = Cvterm.objects.get(cv__name='sequence', name='gene')
+    genes = Feature.objects.values('organism__genus', 'organism__species')
+    genes = genes.filter(type_id=gene_cvterm.cvterm_id)
+    genes = genes.annotate(Count('organism_id'))
+
     data = {
         'Controled vocabularies': cv,
+        'Chromosomes': chrs,
+        'Scaffolds': scaffs,
+        'Genes': genes,
     }
 
     return render(request, 'stats.html', {'context': data})
