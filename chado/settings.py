ae42dbb36946cb6083f0395c3e7bbd73b6ff155f
azneto <azneto@gmail.com>
2018-03-15 11:07:12 -0300

    fix to handle setting with no ROOT_URLCONF

--- chado/settings.py
+++ chado/settings.py
@@ -4,10 +4,11 @@ from importlib import import_module
 
 def patch_root_urlconf():
     from django.conf.urls import include, url
-    urlconf_module = import_module(settings.ROOT_URLCONF)
-    urlconf_module.urlpatterns = [
-        url('chado/', include('chado.urls')),
-    ] + urlconf_module.urlpatterns
+    if hasattr(settings, 'ROOT_URLCONF'):
+        urlconf_module = import_module(settings.ROOT_URLCONF)
+        urlconf_module.urlpatterns = [
+            url('chado/', include('chado.urls')),
+        ] + urlconf_module.urlpatterns
 
 
 def patch_all():
