ebe5ca8f3ab133db5dd1fcbcf4fec481185a5403
azneto <azneto@gmail.com>
2018-03-19 11:06:40 -0300

    testing if migrations ran

--- chado/apps.py
+++ chado/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 from chado import settings as dt_settings
+from django.db.utils import ProgrammingError
+import warnings
 
 
 class ChadoConfig(AppConfig):
@@ -7,4 +9,9 @@ class ChadoConfig(AppConfig):
     verbose_name = 'Django Chado'
 
     def ready(self):
-        dt_settings.patch_all()
+        try:
+            dt_settings.patch_all()
+        except ProgrammingError as e:
+            if str(e).startswith('relation "cvterm" does not exist'):
+                warnings.warn("You need to run: 'python manage.py migrate'")
+            pass
