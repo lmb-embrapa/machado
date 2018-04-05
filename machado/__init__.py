0354ee86a69776d68b99930a563dce522ad76b89
mmudado@gmail.com <m351290@desk6455.cnptia.embrapa.br>
2018-04-05 15:55:51 -0300

    added licensing information

--- machado/__init__.py
+++ machado/__init__.py
@@ -1,2 +1,8 @@
+# Copyright 2018 by Embrapa.  All rights reserved.
+#
+# This code is part of the machado distribution and governed by its
+# license. Please see the LICENSE.txt and README.md files that should
+# have been included as part of this package for licensing information.
+
 default_app_config = 'machado.apps.MachadoConfig'
 urls = 'machado.urls'
--- machado/models.py
+++ machado/models.py
@@ -1,3 +1,9 @@
+# Copyright 2018 by Embrapa.  All rights reserved.
+#
+# This code is part of the machado distribution and governed by its
+# license. Please see the LICENSE.txt and README.md files that should
+# have been included as part of this package for licensing information.
+
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
