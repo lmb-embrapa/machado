2eb8fc2433ef6a16fe0bc857c5a111db554d68d4
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2018-05-14 10:46:28 -0300

    views.stats: minor improvements

--- machado/views.py
+++ machado/views.py
@@ -34,39 +34,70 @@ def index(request):
 
 def stats(request):
     """General stats."""
-    cvs = Cvterm.objects.values(key=F('cv__name'))
-    cvs = cvs.values('key')
-    cvs = cvs.annotate(count=Count('key'))
-    cvs = cvs.filter(count__gt=5)
-    cvs = cvs.order_by('key')
+    data = dict()
 
-    chr_cvterm = Cvterm.objects.get(cv__name='sequence', name='chromosome')
-    chrs = Feature.objects.filter(type_id=chr_cvterm.cvterm_id)
-    chrs = chrs.annotate(key=Concat(
-        'organism__genus', Value(' '), 'organism__species'))
-    chrs = chrs.values('key')
-    chrs = chrs.annotate(count=Count('key'))
+    try:
+        cvs = Cvterm.objects.values(key=F('cv__name'))
+        cvs = cvs.values('key')
+        cvs = cvs.annotate(count=Count('key'))
+        cvs = cvs.filter(count__gt=5)
+        cvs = cvs.order_by('key')
+        if cvs:
+            data.update({'Controlled vocabularies': cvs})
+    except ObjectDoesNotExist:
+        pass
 
-    scaff_cvterm = Cvterm.objects.get(cv__name='sequence', name='assembly')
-    scaffs = Feature.objects.filter(type_id=scaff_cvterm.cvterm_id)
-    scaffs = scaffs.annotate(key=Concat(
-        'organism__genus', Value(' '), 'organism__species'))
-    scaffs = scaffs.values('key')
-    scaffs = scaffs.annotate(count=Count('key'))
+    try:
+        chr_cvterm = Cvterm.objects.get(
+            cv__name='sequence', name='chromosome')
+        chrs = Feature.objects.filter(type_id=chr_cvterm.cvterm_id)
+        chrs = chrs.annotate(key=Concat(
+            'organism__genus', Value(' '), 'organism__species'))
+        chrs = chrs.values('key')
+        chrs = chrs.annotate(count=Count('key'))
+        if chrs:
+            data.update({'Chromosomes': chrs})
+    except ObjectDoesNotExist:
+        pass
 
-    gene_cvterm = Cvterm.objects.get(cv__name='sequence', name='gene')
-    genes = Feature.objects.filter(type_id=gene_cvterm.cvterm_id)
-    genes = genes.annotate(key=Concat(
-        'organism__genus', Value(' '), 'organism__species'))
-    genes = genes.values('key')
-    genes = genes.annotate(count=Count('key'))
+    try:
+        scaff_cvterm = Cvterm.objects.get(
+            cv__name='sequence', name='assembly')
+        scaffs = Feature.objects.filter(type_id=scaff_cvterm.cvterm_id)
+        scaffs = scaffs.annotate(key=Concat(
+            'organism__genus', Value(' '), 'organism__species'))
+        scaffs = scaffs.values('key')
+        scaffs = scaffs.annotate(count=Count('key'))
+        if scaffs:
+            data.update({'Scaffolds': scaffs})
+    except ObjectDoesNotExist:
+        pass
 
-    data = {
-        'Controled vocabularies': cvs,
-        'Chromosomes': chrs,
-        'Scaffolds': scaffs,
-        'Genes': genes,
-    }
+    try:
+        gene_cvterm = Cvterm.objects.get(
+            cv__name='sequence', name='gene')
+        genes = Feature.objects.filter(type_id=gene_cvterm.cvterm_id)
+        genes = genes.annotate(key=Concat(
+            'organism__genus', Value(' '), 'organism__species'))
+        genes = genes.values('key')
+        genes = genes.annotate(count=Count('key'))
+        if genes:
+            data.update({'Genes': genes})
+    except ObjectDoesNotExist:
+        pass
+
+    try:
+        protein_cvterm = Cvterm.objects.get(
+            cv__name='sequence', name='polypeptide')
+        proteins = Feature.objects.filter(type_id=protein_cvterm.cvterm_id)
+        proteins = proteins.annotate(key=Concat(
+            'organism__genus', Value(' '), 'organism__species'))
+        proteins = proteins.values('key')
+        proteins = proteins.annotate(count=Count('key'))
+        if proteins:
+            data.update({'Proteins': proteins})
+    except ObjectDoesNotExist:
+        pass
 
     return render(request, 'stats.html', {'context': data})
 
