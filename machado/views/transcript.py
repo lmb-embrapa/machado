76990c648b6a6e32d43e69bc0afec9f9d0902768
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2018-12-11 13:04:40 -0200

    add settings variables to indicate the JBrowse URL and update documentation

--- machado/views/transcript.py
+++ machado/views/transcript.py
@@ -6,6 +6,7 @@
 
 """transcript views."""
 
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from machado.loaders.common import retrieve_ontology_term
@@ -23,6 +24,7 @@ def get_queryset(request):
 
     feature = dict()
     feature['feature_id'] = feature_id
+    feature['organism'] = request.session.get('current_organism_name')
     if request.GET.get('feature_id') is not None:
         feature_obj = Feature.objects.get(
             feature_id=feature_id,
@@ -45,12 +47,30 @@ def get_queryset(request):
         locations = Featureloc.objects.filter(feature_id=feature_id)
         feature['locations'] = list()
         for location in locations:
+            jbrowse_url = None
+            if hasattr(settings, 'MACHADO_JBROWSE_URL'):
+                if hasattr(settings, 'MACHADO_JBROWSE_OFFSET'):
+                    offset = settings.MACHADO_JBROWSE_OFFSET
+                else:
+                    offset = 1000
+                loc = '{}:{}..{}'.format(
+                    Feature.objects.get(
+                        feature_id=location.srcfeature_id).uniquename,
+                    location.fmin - offset,
+                    location.fmax + offset,
+                )
+                jbrowse_url = '{}/?data=data/{}&loc={}'\
+                              '&tracklist=0&nav=0&overview=0'.format(
+                                  settings.MACHADO_JBROWSE_URL,
+                                  request.session.get('current_organism_name'),
+                                  loc)
             feature['locations'].append({
                 'start': location.fmin,
                 'end': location.fmax,
                 'strand': location.strand,
                 'ref': Feature.objects.get(
                     feature_id=location.srcfeature_id).uniquename,
+                'jbrowse_url': jbrowse_url,
             })
 
         return render(request,
