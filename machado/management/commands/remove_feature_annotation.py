6d6bfc02d62d5f703c4858ff9f9748cb140bb8d1
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2019-02-04 09:42:50 -0200

    adds remove_feature_annotation command

---
+++ machado/management/commands/remove_feature_annotation.py
@@ -0,0 +1,56 @@
+# Copyright 2018 by Embrapa.  All rights reserved.
+#
+# This code is part of the machado distribution and governed by its
+# license. Please see the LICENSE.txt and README.md files that should
+# have been included as part of this package for licensing information.
+
+"""Remove feature annotation."""
+from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist
+from machado.loaders.common import retrieve_organism
+from machado.models import Cvterm, Featureprop
+
+
+class Command(BaseCommand):
+    """Remove organism."""
+
+    help = 'Remove organism'
+
+    def add_arguments(self, parser):
+        """Define the arguments."""
+        parser.add_argument("--organism", help="Species name (eg. Homo "
+                            "sapiens, Mus musculus)",
+                            required=True,
+                            type=str)
+        parser.add_argument("--cvterm", help="cvterm.name from cv "
+                            "feature_property. (eg. display, note, product, "
+                            "alias, ontology_term)",
+                            required=True,
+                            type=str)
+
+    def handle(self,
+               organism: str,
+               cvterm: str,
+               verbosity: int=1,
+               **options):
+        """Execute the main function."""
+        try:
+            organism_obj = retrieve_organism(organism)
+        except ObjectDoesNotExist:
+                raise CommandError('Organism does not exist in database!')
+
+        try:
+            cvterm_obj = Cvterm.objects.get(name=cvterm,
+                                            cv__name='feature_property')
+        except ObjectDoesNotExist:
+                raise CommandError('cvterm does not exist in database!')
+
+        feature_props = Featureprop.objects.filter(
+            type=cvterm_obj, feature__organism=organism_obj)
+
+        count = feature_props.count()
+        feature_props.delete()
+
+        if verbosity > 0:
+            self.stdout.write(self.style.SUCCESS(
+                '{} removed'.format(count)))
