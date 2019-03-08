f9f3b024bc4210d83e127306560d05dcc0f7236a
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2019-03-08 10:24:34 -0300

    tests for management.commands.load_sequence_ontology

---
+++ machado/tests/test_commands_load_sequence_ontology.py
@@ -0,0 +1,28 @@
+# Copyright 2018 by Embrapa.  All rights reserved.
+#
+# This code is part of the machado distribution and governed by its
+# license. Please see the LICENSE.txt and README.md files that should
+# have been included as part of this package for licensing information.
+
+"""Tests command load sequence ontology."""
+
+import os
+from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from machado.models import Cvterm
+
+
+class CommandTest(TestCase):
+    """Tests Loaders - SequenceLoader."""
+
+    def test_insert_organism(self):
+        """Tests - insert organism."""
+        # test insert organism
+        directory = os.path.dirname(os.path.abspath(__file__))
+        file = os.path.join(directory, 'data', 'so_trunc.obo')
+        call_command("load_sequence_ontology",
+                     "--file={}".format(file),
+                     "--verbosity=0")
+        self.assertTrue(Cvterm.objects.get(name='gene'))
