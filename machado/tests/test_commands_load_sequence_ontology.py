13ceca6aff64796c3f8cd0d1f5aaf3b84f027ca5
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2019-03-08 11:10:28 -0300

    tests for management.commands.load_sequence_ontology

--- machado/tests/test_commands_load_sequence_ontology.py
+++ machado/tests/test_commands_load_sequence_ontology.py
@@ -8,6 +8,7 @@
 
 import os
 from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from machado.models import Cvterm
@@ -16,9 +17,9 @@ from machado.models import Cvterm
 class CommandTest(TestCase):
     """Tests Loaders - SequenceLoader."""
 
-    def test_insert_organism(self):
+    def test_load_sequence_ontology(self):
         """Tests - insert organism."""
-        # test insert organism
+        # test load sequence ontology
         directory = os.path.dirname(os.path.abspath(__file__))
         file = os.path.join(directory, 'data', 'so_trunc.obo')
         call_command("load_sequence_ontology",
@@ -30,3 +31,17 @@ class CommandTest(TestCase):
         with self.assertRaises(CommandError):
             call_command("load_sequence_ontology",
                          "--file=does_not_exist")
+
+        # test remove sequence ontology
+        call_command("remove_ontology",
+                     "--name=sequence",
+                     "--verbosity=0")
+        with self.assertRaises(ObjectDoesNotExist):
+            Cvterm.objects.get(name='gene')
+
+        # test remove ontology does not exist
+        with self.assertRaisesMessage(
+                CommandError, 'Cannot remove \'sequence\' (not registered)'):
+            call_command("remove_ontology",
+                         "--name=sequence",
+                         "--verbosity=0")
