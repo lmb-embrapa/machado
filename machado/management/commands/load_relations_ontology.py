98effb53b9c8820ac9a290fb4c3f803084053a9c
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2019-03-11 16:44:54 -0300

    additional test for commands.load_relations_ontology

--- machado/management/commands/load_relations_ontology.py
+++ machado/management/commands/load_relations_ontology.py
@@ -48,7 +48,9 @@ class Command(BaseCommand):
         if verbosity > 0:
             self.stdout.write('Loading typedefs')
 
-        for data in tqdm(G.graph['typedefs']):
+        for data in tqdm(G.graph['typedefs'],
+                         disable=False if verbosity > 0 else True):
             ontology.store_type_def(data)
 
-        self.stdout.write(self.style.SUCCESS('Done'))
+        if verbosity > 0:
+            self.stdout.write(self.style.SUCCESS('Done'))
