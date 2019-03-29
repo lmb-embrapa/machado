038197861494f62c14b657c190bbefbcff7368ef
Mauricio de Alvarenga Mudadu <mmudado@gmail.com>
2019-03-29 11:22:18 -0300

    Remove_relationship paralellized and minor modifications to caching in loading clusters

--- machado/management/commands/remove_relationship.py
+++ machado/management/commands/remove_relationship.py
@@ -11,6 +11,7 @@ from machado.loaders.exceptions import ImportingError
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.utils import IntegrityError
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 import os
 
@@ -26,9 +27,24 @@ class Command(BaseCommand):
                             help="name of the file (e.g.: groups.txt",
                             required=True,
                             type=str)
+        parser.add_argument("--cpu",
+                            help="Number of threads",
+                            default=1,
+                            type=int)
+
+    def remove_fr(self,
+                  frp: FeatureRelationshipprop):
+        try:
+            FeatureRelationship.objects.filter(
+                   feature_relationship_id=frp.feature_relationship_id
+                   ).delete()
+            frp.delete()
+        except Exception as e:
+            raise(e)
 
     def handle(self,
                file: str,
+               cpu: int = 1,
                verbosity: int = 0,
                **options):
         """Execute the main function."""
@@ -39,30 +55,42 @@ class Command(BaseCommand):
         except IntegrityError as e:
             raise ImportingError(e)
         filename = os.path.basename(file)
+        pool = ThreadPoolExecutor(max_workers=cpu)
+        tasks = list()
         try:
+            if verbosity > 0:
+                self.stdout.write(
+                        'Removing FeatureRelationships with filename {}'
+                        .format(filename))
             frps = FeatureRelationshipprop.objects.filter(
                     value=filename,
                     type_id=cvterm_contained_in.cvterm_id)
             if verbosity > 0:
-                self.stdout.write(
-                        'Deleting every relationship relations from {}'
-                        .format(filename))
-                # get all feature_relationship_id and
-                # remove all feature_relationshipprop
+                self.stdout.write('Preprocessing...')
                 for frp in tqdm(frps, total=len(frps)):
-                    fr = FeatureRelationship.objects.get(
-                           feature_relationship_id=frp.feature_relationship_id)
-                    frp.delete()
-                    fr.delete()
+                    tasks.append(pool.submit(
+                              self.remove_fr,
+                              frp=frp,
+                              ))
             else:
                 for frp in frps:
-                    fr = FeatureRelationship.objects.get(
-                           feature_relationship_id=frp.feature_relationship_id)
-                    frp.delete()
-                    fr.delete()
-            # remove all feature_relationships
+                    tasks.append(pool.submit(
+                              self.remove_fr,
+                              frp=frp,
+                              ))
+
+            if verbosity > 0:
+                self.stdout.write('Removing...')
+                for task in tqdm(as_completed(tasks), total=len(tasks)):
+                    if task.result():
+                        raise(task.result())
+            else:
+                for task in as_completed(tasks):
+                    if task.result():
+                        raise(task.result())
             if verbosity > 0:
                 self.stdout.write(self.style.SUCCESS('Done'))
+            pool.shutdown()
         except IntegrityError as e:
             raise CommandError(
                     'It\'s not possible to delete every record. You must '
