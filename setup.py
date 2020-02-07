a6ba0fd412e53b053030a89a45d92fe1d12537d6
Adhemar Zerlotini Neto <adhemar.zerlotini@embrapa.br>
2020-02-07 10:04:32 -0300

    configures setup.py to require specific library versions

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     ],
     scripts=["bin/fixChadoModel.py"],
     install_requires=[
-        "django>=2.0.1",
+        "django>=2.2,<3",
         "psycopg2-binary>=2.7.4",
         "biopython>=1.72",
         "obonet>=0.2.2",
