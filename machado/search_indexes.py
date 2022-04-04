1a702cd3d789d0ab4ca15262faa31d9f91c6fa30
azneto <azneto@gmail.com>
2022-04-04 11:03:51 -0300

    adds doi to the index

--- machado/search_indexes.py
+++ machado/search_indexes.py
@@ -34,7 +34,7 @@ class FeatureIndex(indexes.SearchIndex, indexes.Indexable):
     name = indexes.CharField(model_attr="name", faceted=True, null=True)
     analyses = indexes.MultiValueField(faceted=True)
     display = indexes.CharField(faceted=True, null=True)
-    annotation = indexes.MultiValueField(faceted=True)
+    doi = indexes.MultiValueField(faceted=True)
     relationship = indexes.MultiValueField(indexed=False)
     if Featureprop.objects.filter(
         type__name="orthologous group", type__cv__name="feature_property"
@@ -145,6 +145,10 @@ class FeatureIndex(indexes.SearchIndex, indexes.Indexable):
         for annotation in obj.get_annotation():
             keywords.add(annotation)
 
+        # DOI
+        for doi in obj.get_doi():
+            keywords.add(doi)
+
         # Expression samples
         for sample in obj.get_expression_samples():
             keywords.add(sample.get("assay_name"))
@@ -181,11 +185,11 @@ class FeatureIndex(indexes.SearchIndex, indexes.Indexable):
         self.temp = " ".join(keywords)
         return " ".join(keywords)
 
-    def prepare_annotation(self, obj):
-        """Prepare annotation."""
+    def prepare_doi(self, obj):
+        """Prepare DOI."""
         result = list()
-        for annotation in obj.get_annotation():
-            result.append(annotation)
+        for doi in obj.get_doi():
+            result.append(doi)
         return result
 
     def prepare_orthology(self, obj):
