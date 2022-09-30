0a6805e6d290bc15192e1d576869d284e6dc4650
Nick Booher <njbooher@iastate.edu>
2022-09-30 16:43:11 -0500

    stash it in version instead

--- machado/loaders/feature.py
+++ machado/loaders/feature.py
@@ -137,7 +137,7 @@ class FeatureLoader(FeatureLoaderBase):
 
         try:
             dbxref, created = Dbxref.objects.get_or_create(
-                db=self.db, accession=attrs_id, description=self.filename
+                db=self.db, accession=attrs_id, version=self.filename
             )
             Dbxrefprop.objects.get_or_create(
                 dbxref=dbxref,
