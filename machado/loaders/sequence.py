96c43d2560e0b60075276b1de3b9ed49605a5c82
adhemar <adhemar.zerlotini@embrapa.br>
2023-02-07 10:42:00 -0300

    do not store the file name in the version field to allow duplicate name for different organisms

--- machado/loaders/sequence.py
+++ machado/loaders/sequence.py
@@ -72,7 +72,7 @@ class SequenceLoader(object):
 
         try:
             dbxref, created = Dbxref.objects.get_or_create(
-                db=self.db, accession=seq_obj.id, version=self.filename
+                db=self.db, accession=seq_obj.id
             )
             Dbxrefprop.objects.get_or_create(
                 dbxref=dbxref,
