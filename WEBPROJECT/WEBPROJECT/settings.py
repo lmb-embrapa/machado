28122ad106d1da65cf02d7f7fd33b369804e0104
rickxz <herick.victor.rodrigues@hotmail.com>
2025-05-15 20:25:15 +0100

    chore: obter configurações de conexão do db pelo env

--- WEBPROJECT/WEBPROJECT/settings.py
+++ WEBPROJECT/WEBPROJECT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,9 +106,9 @@ DATABASES = {
       # Lembre-se de usar os valores criados na configuração do PostgreSQL.
       'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'machado',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': ''
       }
